92525a4945ea3e2be73ea3eb5309f393e4621ad6
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-17 15:54:58 +0900

    add mixin

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,21 +1,40 @@
+
+const videos = [{
+    title: "apple",
+    color: "red",
+    id: 1,
+},
+{
+    title: "banana",
+    color: "yellow",
+    id: 2,
+},
+{
+    title: "orange",
+    color: "orange",
+    id: 3,
+},
+]
+
+
 export const home = (req, res) => {
-    return res.render("home");
+    return res.render("home", { pageTitle: "Home", videos });
 }
 
 export const join = (req, res) => {
-    return res.render("join");
+    return res.render("join", { pageTitle: "Join" });
 }
 
 export const login = (req, res) => {
-    return res.render("login");
+    return res.render("login", { pageTitle: "Login" });
 }
 
 export const logout = (req, res) => {
-    return res.render("logout");
+    return res.render("logout", { pageTitle: "Logout" });
 }
 export const userEdit = (req, res) => {
-    return res.render("userEdit");
+    return res.render("userEdit", { pageTitle: "UserEdit" });
 }
 export const userProfile = (req, res) => {
-    return res.render("userProfile");
+    return res.render("userProfile", { pageTitle: "UserProfile" });
 }
--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,12 +1,12 @@
 export const search = (req, res) => {
-    return res.render("search");
+    return res.render("search", { pageTitle: "Search" });
 }
 export const videoEdit = (req, res) => {
-    return res.render("videoEdit");
+    return res.render("videoEdit", { pageTitle: "VideoEdit" });
 }
 export const videoUpload = (req, res) => {
-    return res.render("videoUpload");
+    return res.render("videoUpload", { pageTitle: "VideoUpload" });
 }
 export const videoProfile = (req, res) => {
-    return res.render("videoProfile");
+    return res.render("videoProfile", { pageTitle: "VideoProfile" });
 }
