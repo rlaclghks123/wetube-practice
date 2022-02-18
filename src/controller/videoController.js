710b993aacb9782e237a8f765620bdc1bbee871a
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-18 20:08:10 +0900

    watch, videoEdit 페이지 작성

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,8 +1,33 @@
+
+const videos = [{
+    title: "apple",
+    color: "red",
+    id: 1,
+},
+{
+    title: "banana",
+    color: "yellow",
+    id: 2,
+},
+{
+    title: "orange",
+    color: "orange",
+    id: 3,
+},
+]
+
 export const search = (req, res) => {
     return res.render("search", { pageTitle: "Search" });
 }
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: "watch", video });
+}
 export const videoEdit = (req, res) => {
-    return res.render("videoEdit", { pageTitle: "VideoEdit" });
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("videoEdit", { pageTitle: "VideoEdit", video });
 }
 export const videoUpload = (req, res) => {
     return res.render("videoUpload", { pageTitle: "VideoUpload" });
