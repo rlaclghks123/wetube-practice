2704027c3b5b6f7631a4642275e33e06ff18a93c
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-19 15:38:13 +0900

    mixin, model Video수정

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-const videoSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    descripton: { type: String, required: true },
+const videoSchema = new mongoose.Schema({
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
     createdAt: { type: Number, required: true, default: Date.now },
+    hashtags: [{ type: String, trim: true }],
     meta: {
         views: { type: Number, default: 0, required: true },
         rating: { type: Number, default: 0, required: true }
