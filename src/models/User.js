b5ec149295e6f58fb4d32eec0dd51b059cc4ff09
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-21 12:02:19 +0900

    User part join-2
    password Hashing with bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema({
     username: { type: String, unique: true, required: true },
@@ -7,6 +8,11 @@ const userSchema = mongoose.Schema({
     password: { type: String, required: true },
 });
 
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 5);
+    }
+})
 const User = mongoose.model("User", userSchema);
 
 export default User;
