d4b9682a9c86c11eca1c43ec01a50b4e6b25fa21
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-21 12:09:42 +0900

    User part-3
    username or email 존재시 에러발생 처리

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -6,7 +6,15 @@ export const getJoin = (req, res) => {
 
 export const postJoin = async (req, res) => {
     const { username, email, name, password, password2 } = req.body;
-    if (password === password2) {
+    if (password != password2) {
+        return res.status(400).render("join", { errorMessage: "password was not correct" });
+    }
+    const exist = await User.exists({ $or: [{ username }, { email }] });
+    if (exist) {
+        return res.status(400).render("join", { pageTitle: "join", errorMessage: "This username/email is already taken" })
+    }
+
+    try {
         await User.create({
             username,
             email,
@@ -15,9 +23,12 @@ export const postJoin = async (req, res) => {
             password2
         });
         return res.redirect("/login");
-    } else {
-        return res.render("join", { errorMessage: "password was not correct" });
     }
+    catch (error) {
+        res.status(400).render("join", { pageTitle: "join", errorMessage: error._message });
+    }
+
+
 }
 
 export const login = (req, res) => {
