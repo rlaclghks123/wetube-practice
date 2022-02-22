d4e9c3552f94f8adea6c42a3418b73ddc371a9b6
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-22 23:08:48 +0900

    user part-5 / login-2
    router,controller 작성중

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -31,7 +31,21 @@ export const postJoin = async (req, res) => {
 
 }
 
-export const login = (req, res) => {
+export const getLogin = (req, res) => {
+    return res.render("login", { pageTitle: "Login" });
+}
+
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(400).render("login", { pageTitle: "Error", errorMessage: "useraname is not exists " });
+    }
+    const ok = await bcypts.compare(password, user.password);
+    if (!ok) {
+        return res.status(400).render("login", { pageTitle: "Error", errorMessage: "password is not exists " });
+    }
+
     return res.render("login", { pageTitle: "Login" });
 }
 
--- src/router/globalRouter.js
+++ src/router/globalRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controller/userController";
+import { getJoin, postJoin, getLogin, postLogin } from "../controller/userController";
 import { home, search } from "../controller/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.route("/join").get(getJoin).post(postJoin);
-globalRouter.get("/login", login);
+globalRouter.route("/login").get(getLogin).post(postLogin);
 globalRouter.get("/search", search);
 
 export default globalRouter;
