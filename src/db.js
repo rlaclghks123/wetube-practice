1a4b32118ad59eae3dc7e31fb1ab01d34444e02b
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-23 22:57:03 +0900

    User part - login-3
    session,cookie-1

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.DBURL, { useNewUrlParser: true });
+mongoose.connect(process.env.API_KEY, { useNewUrlParser: true });
 
 const db = mongoose.connection
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./router/globalRouter";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
@@ -17,10 +19,15 @@ const logger = morgan("dev");
 // setting
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-
 // middle ware
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.COOKIT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.API_KEY })
+}))
 
 // Router
 app.use("/", globalRouter);
