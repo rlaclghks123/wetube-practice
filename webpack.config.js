be6890451c36f30a74e2690c236eff95c638e1f1
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-04 23:22:42 +0900

    webpack2 scss ->css 로 변환

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
         main: "./src/clients/js/main.js",
     },
     mode: "development",
+    plugins: [new MiniCssExtractPlugin({ filename: "css/styles.css" })],
     output: {
         filename: "js/[name].js",
         path: path.resolve(__dirname, "assets"),
@@ -20,6 +22,10 @@ module.exports = {
                         presets: [["@babel/preset-env", { targets: "defaults" }]],
                     },
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     }
