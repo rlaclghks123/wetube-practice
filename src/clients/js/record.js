c8675303233758f30878fa7b9c9853cc6f8ee19b
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-08 16:32:30 +0900

    video record

--- src/clients/js/record.js
+++ src/clients/js/record.js
@@ -1,7 +1,34 @@
 const video = document.querySelector("#preview");
-
+const actionBtn = document.querySelector("#actionBtn");
 
 let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+
+}
+
+const handleStart = () => {
+    actionBtn.innerText = "Recording..."
+    actionBtn.disabled = true;
+    actionBtn.removeEventListener("click", handleStart);
+
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        videoFile = URL.createObjectURL(event.data);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
+        actionBtn.innerText = "Donwload";
+        actionBtn.disabled = false;
+        actionBtn.addEventListener("click", handleDownload);
+    };
+    recorder.start();
+    setTimeout(() => { recorder.stop() }, 3000);
+}
+
 const init = async () => {
     stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
@@ -11,3 +38,5 @@ const init = async () => {
     video.play();
 };
 init();
+
+actionBtn.addEventListener("click", handleStart);
