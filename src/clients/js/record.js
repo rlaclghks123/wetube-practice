b00ab7bfad563b0af8f181805e98bea44c119a48
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-09 17:12:35 +0900

    ffmpeg를 사용하여 jpg파일로 변환

--- src/clients/js/record.js
+++ src/clients/js/record.js
@@ -18,19 +18,28 @@ const handleDownload = async () => {
     ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
 
     await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");
 
     const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
 
     const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+    const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
     const mp4Url = URL.createObjectURL(mp4Blob);
-
+    const thumbUrl = URL.createObjectURL(thumbBlob);
 
 
     const a = document.createElement("a");
     a.href = mp4Url;
     a.download = "MyRecording.mp4";
     document.body.appendChild(a);
+    a.click();
+
+    const a1 = document.createElement("a");
+    a.href = thumbUrl;
+    a.download = "MyThumbnail.jpg";
+    document.body.appendChild(a1);
     a.click();
 }
 
