a2aed2f0b6305c9d138f6c48028b3174d9a66e81
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-09 16:04:48 +0900

    ffmpeg 사용하여 mp4로 변환

--- src/clients/js/record.js
+++ src/clients/js/record.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const video = document.querySelector("#preview");
 const actionBtn = document.querySelector("#actionBtn");
 
@@ -5,10 +7,29 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+
+    const ffmpeg = createFFmpeg({
+        log: true,
+        corePath: "https://unpkg.com/@ffmpeg/core@0.8.5/dist/ffmpeg-core.js",
+    });
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+    const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+
+    const mp4Url = URL.createObjectURL(mp4Blob);
+
+
+
     const a = document.createElement("a");
-    a.href = videoFile;
-    a.download = "recording.webm";
+    a.href = mp4Url;
+    a.download = "MyRecording.mp4";
     document.body.appendChild(a);
     a.click();
 }
